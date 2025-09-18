2f963dcc61df4075a294345888f8d264b00fa000
Daniel Stark <starkunison@gmail.com>
2025-09-18 17:28:03 +0300

    🛠 Fix: добавлен настоящий Gradle Wrapper

--- gradlew.bat
+++ gradlew.bat
@@ -1,3 +1,5 @@
 @ECHO OFF
 SET DIR=%~dp0
-java -jar "%DIR%\gradle\wrapper\gradle-wrapper.jar" %*
+SET APP_BASE_NAME=%~n0
+SET DEFAULT_JVM_OPTS=
+java %DEFAULT_JVM_OPTS% -cp "%DIR%\gradle\wrapper\gradle-wrapper.jar" org.gradle.wrapper.GradleWrapperMain %*
